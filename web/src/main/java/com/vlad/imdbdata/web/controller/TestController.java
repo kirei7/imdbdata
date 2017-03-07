55f658e8d8dddddc0af1c580a54ec7170b1e9c91
kirei7 <vlad.sereda.y@gmail.com>
2017-03-07 20:52:36 +0200

    Add logging

--- web/src/main/java/com/vlad/imdbdata/web/controller/TestController.java
+++ web/src/main/java/com/vlad/imdbdata/web/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.vlad.imdbdata.web.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,8 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/test")
 public class TestController {
 
+    private final Logger LOGGER = LoggerFactory.getLogger(TestController.class);
+
     @RequestMapping("/")
     public String helloWorld() {
+        LOGGER.debug("hello world executed");
         return "Hello world";
     }
 }
