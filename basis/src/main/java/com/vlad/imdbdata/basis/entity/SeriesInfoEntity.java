ef867e671eadffe0991b29a3f6f1a5f64a27d1ab
kirei7 <vlad.sereda.y@gmail.com>
2017-03-12 18:14:46 +0200

    Merge modules v.2

--- basis/src/main/java/com/vlad/imdbdata/basis/entity/SeriesInfoEntity.java
+++
@@ -1,8 +0,0 @@
-package com.vlad.imdbdata.basis.entity;
-
-import java.util.List;
-
-public class SeriesInfoEntity extends MediaInfoEntity {
-
-    private List<List<MediaInfoEntity>> seriesList;
-}
